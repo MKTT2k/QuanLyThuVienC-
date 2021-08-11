3edcb7492d32eff10e520566026d816d58169ccd
MKTT2k <63504561+MKTT2k@users.noreply.github.com>
2021-08-11 23:06:51 +0700

    First
    GUI

---
+++ QLThuVienCSharp/QLThuVienCSharp/GUI/ManagerForm/QlMuonTra/fQLDocGia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLThuVienCSharp.GUI.ManagerForm.QlMuonTra
+{
+    public partial class fQLDocGia : Form
+    {
+        public fQLDocGia()
+        {
+            InitializeComponent();
+        }
+    }
+}
