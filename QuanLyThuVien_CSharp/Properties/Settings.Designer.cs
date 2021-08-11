c66e19be6a9870484d2306a9e18534f709068099
Ha Thanh Hoang <hathanhhoang842000@gmail.com>
2021-08-11 19:58:53 +0700

    result 11/08

--- QuanLyThuVien_CSharp/Properties/Settings.Designer.cs
+++ QuanLyThuVien_CSharp/Properties/Settings.Designer.cs
@@ -8,21 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace QuanLyThuVien_CSharp.Properties
-{
+namespace QuanLyThuVien_CSharp.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SqlExpress;Initial Catalog=QuanLyThuVienCSharp;Integrated Security=" +
+            "True")]
+        public string QuanLyThuVienCSharpConnectionString {
+            get {
+                return ((string)(this["QuanLyThuVienCSharpConnectionString"]));
             }
         }
     }
